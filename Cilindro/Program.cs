92fc17cc42d5e212917b54bfe128f968b1e819ae
DiogoMeiraFreire <didi9934@gmail.com>
2024-02-28 19:39:20 +0000

    Added Volume calculation

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -6,7 +6,14 @@ namespace Cilindro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Indique a altura do cilindro: ");
+            float a = float.Parse(Console.ReadLine());
+            Console.Write("Indique o raio do cilindro: ");
+            float r = float.Parse(Console.ReadLine());
+
+            float Volume = MathF.PI * MathF.Pow(r, 2) * a;
+
+            Console.WriteLine(Volume);
         }
     }
 }
