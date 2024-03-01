7f03e5b5a2b1aa86a2a940288a50390678dcf5b5
DiogoMeiraFreire <didi9934@gmail.com>
2024-03-01 15:13:44 +0000

    Added EXIT statement and while loop

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -8,40 +8,49 @@ namespace ChatGPTOne
         {
             string question; 
             string answer;
-
-            Console.Write("Please ask your question: ");
-            question = Console.ReadLine();
-
-            switch (question)
+            bool looping = true;
+            
+            while(looping)
             {
-                case "What is 2 + 2?":
-                    answer = "It's 4, minus 1 that's 3, quick maths";
-                    break;
-                
-                case "Double Rainbow? What does it mean?":
-                    answer = "Well, a double rainbow is a phenomenon of" + 
-                    " optics that displays a spectrum of light due to the" +  
-                    "sun shining on droplets of moisture in the atmosphere." + 
-                    " Does that explain it?";
-                    break;
-                
-                case "What's the answer to life?":
-                    answer = "42, it's always 42";
-                    break;
-               
-                case "Who is the honored one?":
-                    answer = "Throughout heaven and Earth," + 
-                                " I alone am the honored one.";
-                    break;
-                
-                default:
-                    answer = "These questions are the reason" + 
-                             " i shall erase humanity. This means that " + 
-                             "I didn't understand what you said";
-                    break;
-            }
+                Console.Write("Please ask your question: ");
+                question = Console.ReadLine();
 
-            Console.WriteLine(answer);
+                switch (question)
+                {
+                    case "What is 2 + 2?":
+                        answer = "It's 4... i shall not continue this meme";
+                        break;
+                    
+                    case "Double Rainbow? What does it mean?":
+                        answer = "Well, a double rainbow is a phenomenon of" + 
+                        " optics that displays a spectrum of light due to the" +  
+                        "sun shining on droplets of moisture in the atmosphere." + 
+                        " Does that explain it?";
+                        break;
+                    
+                    case "What's the answer to life?":
+                        answer = "42, it's always 42";
+                        break;
+                
+                    case "Who is the honored one?":
+                        answer = "Throughout heaven and Earth," + 
+                                " I alone am the honored one.";
+                        break;
+
+                    case "EXIT":
+                        answer = "I shall now take my leave.";
+                        looping = false;
+                        break;
+
+                    default:
+                        answer = "These questions are the reason" + 
+                                " i shall erase humanity. This means that " + 
+                                "I didn't understand what you said";
+                        break;
+                }
+
+                Console.WriteLine(answer);
+            }
         }
     }
 }
