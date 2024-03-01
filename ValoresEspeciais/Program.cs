460eb01469674dfc94637fba36f8d333a2c6c72e
DiogoMeiraFreire <didi9934@gmail.com>
2024-03-01 13:36:14 +0000

    Added maximum and minimum values of the variables

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -6,7 +6,126 @@ namespace ValoresEspeciais
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Variables
+
+            // Integers
+            byte    bt;
+            sbyte   sbt;
+            short   sh;
+            ushort  ush;
+            int     i;
+            uint    ui;
+            long    ln;
+            ulong   uln;
+
+            // Chars
+            char    ch;
+
+            // Floats
+            double  db;
+            float   fl;
+            decimal dc;
+
+            // Booleans
+            bool    bl;
+
+            #endregion
+
+            #region Max Integer prints
+
+            Console.WriteLine("Maximum Integer Values:");
+
+            bt = byte.MaxValue;
+            Console.WriteLine("Byte: " + bt);
+            
+            sbt = sbyte.MaxValue;
+            Console.WriteLine("Sbyte: " + sbt);
+            
+            sh = short.MaxValue;
+            Console.WriteLine("Short: " + sh);
+            
+            ush = ushort.MaxValue;
+            Console.WriteLine("Ushort: " + ush);
+            
+            i = int.MaxValue;
+            Console.WriteLine("Int: " + i);
+            
+            ui = uint.MaxValue;
+            Console.WriteLine("Uint: " + ui);
+            
+            ln = long.MaxValue;
+            Console.WriteLine("Long: " + ln);
+            
+            uln = ulong.MaxValue;
+            Console.WriteLine("Ulong: " + uln);           
+            #endregion
+        
+            #region Min Integer prints
+
+            Console.WriteLine("\nMinimum Integer Values:");
+            bt = byte.MinValue;
+            Console.WriteLine("Byte: " + bt);
+            
+            sbt = sbyte.MinValue;
+            Console.WriteLine("Sbyte: " + sbt);
+            
+            sh = short.MinValue;
+            Console.WriteLine("Short: " + sh);
+            
+            ush = ushort.MinValue;
+            Console.WriteLine("Ushort: " + ush);
+            
+            i = int.MinValue;
+            Console.WriteLine("Int: " + i);
+            
+            ui = uint.MinValue;
+            Console.WriteLine("Uint: " + ui);
+            
+            ln = long.MinValue;
+            Console.WriteLine("Long: " + ln);
+            
+            uln = ulong.MinValue;
+            Console.WriteLine("Ulong: " + uln); 
+            #endregion
+        
+            #region Char prints
+
+            Console.WriteLine("\nMaximum Char Value:");
+            ch = char.MaxValue;
+            Console.WriteLine("Char: " + ch);
+
+
+            Console.WriteLine("\nMinimum Char Value:");
+            ch = char.MinValue;
+            Console.WriteLine("Char: " + ch);
+
+            #endregion
+
+            #region Float prints
+            
+            Console.WriteLine("\nMaximum Float Values:");
+            db = double.MaxValue;
+            Console.WriteLine("Double: " + db);
+
+            fl = float.MaxValue;
+            Console.WriteLine("Float: " + fl);
+
+            dc = decimal.MaxValue;
+            Console.WriteLine("Decimal: " + dc);
+// -----------------------------------------------------------------
+            
+            Console.WriteLine("\nMinimum Float Values:");
+            db = double.MinValue;
+            Console.WriteLine("Double: " + db);
+
+            fl = float.MinValue;
+            Console.WriteLine("Float: " + fl);
+
+            dc = decimal.MinValue;
+            Console.WriteLine("Decimal: " + dc);
+
+            #endregion
+
         }
     }
 }
